4c7f2ad7dc171dceb00ff9e9762ccab836974035
iothua <1158078383@qq.com>
2018-11-28 13:46:13 +0800

    1、新增实验场景脚本（用于配置实验参数的）ExperimentNotification.cs。 2、新增灯光配置参数。

---
+++ Assets/MagiCloud/Scripts/Cameras/CameraProperty.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace MagiCloud.Cameras
+{
+    /// <summary>
+    /// 摄像机属性
+    /// </summary>
+    public struct CameraProperty
+    {
+        public Transform parent;
+        public Vector3 localPosition;
+        public Quaternion localRotation;
+        public CameraClearFlags clearFlags;
+        public Color backgroundColor;
+        
+
+        public CameraProperty(Camera camera)
+        {
+            parent = camera.transform.parent;
+            localPosition = camera.transform.localPosition;
+            localRotation = camera.transform.localRotation;
+            clearFlags = camera.clearFlags;
+            backgroundColor = camera.backgroundColor;
+        }
+
+        /// <summary>
+        /// 设置摄像机属性
+        /// </summary>
+        /// <param name="camera"></param>
+        public void SetCameraProperty(Camera camera)
+        {
+            if (parent == null)
+                camera.transform.parent = null;
+            else
+                camera.transform.SetParent(parent);
+
+            camera.transform.localPosition = localPosition;
+            camera.transform.localRotation = localRotation;
+            camera.clearFlags = clearFlags;
+            camera.backgroundColor = backgroundColor;
+        }
+    }
+}
---
+++ Assets/MagiCloud/Scripts/Equipments/ExperimentNotification.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using MagiCloud.Cameras;
+using MagiCloud.Core;
+using MagiCloud.Utility.Lighting;
+
+namespace MagiCloud.Equipments
+{
+    
+    public class ExperimentNotification : MonoBehaviour
+    {
+        public static bool IsExperimentHome;
+        public string ExperimentName;
+
+        public LightingData lightingData;
+        public LightingData normalLighting;
+
+        public bool IsSetLighting;
+
+        public UnityEvent onAwakeEvent, onEnableEvent, onStartEvent, onDisableEvent, onDestoryEvent;
+
+        private CameraProperty normalCamera;
+        public bool isSetCamera;
+        public CameraProperty setCamera; //设置相机位置
+
+        private MBehaviour behaviour;
+
+        private void Awake()
+        {
+            behaviour = new MBehaviour(ExecutionPriority.High, -100, enabled);
+
+            behaviour.OnAwake(() =>
+            {
+                normalCamera = new CameraProperty(MUtility.MainCamera);
+
+                if (isSetCamera)
+                {
+                    setCamera.SetCameraProperty(MUtility.MainCamera);
+                }
+
+                if (onAwakeEvent != null)
+                    onAwakeEvent.Invoke();
+                IsExperimentHome = true;
+            });
+
+            behaviour.OnEnable(() =>
+            {
+                if (onEnableEvent != null)
+                    onEnableEvent.Invoke();
+            });
+
+            behaviour.OnStart(() =>
+            {
+                if (IsSetLighting && lightingData != null)
+                    SystemParameters.SetLighting(lightingData);
+
+                if (onStartEvent != null)
+                    onStartEvent.Invoke();
+            });
+
+            behaviour.OnDisable(() =>
+            {
+                if (onDisableEvent != null)
+                    onDisableEvent.Invoke();
+            });
+
+            behaviour.OnDestroy(() =>
+            {
+                if (onDestoryEvent != null)
+                    onDestoryEvent.Invoke();
+
+                if (normalLighting != null)
+                {
+                    SystemParameters.SetLighting(normalLighting);
+                }
+            });
+        }
+
+        private void OnEnable()
+        {
+            behaviour.IsEnable = true;
+        }
+
+        private void OnDisable()
+        {
+            behaviour.IsEnable = false;
+        }
+
+        private void OnDestroy()
+        {
+            behaviour.OnExcuteDestroy();
+        }
+    }
+}
+
