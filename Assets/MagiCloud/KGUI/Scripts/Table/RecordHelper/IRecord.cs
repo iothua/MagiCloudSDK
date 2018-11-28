7d9ecab44d228a3a7ffa5eb16acad05908d017b1
LiiiiiWr <331718478@qq.com>
2018-11-28 10:14:42 +0800

    关于常规表格的数据写入扩展

---
+++ Assets/MagiCloud/KGUI/Scripts/Table/RecordHelper/IRecord.cs
@@ -0,0 +1,11 @@
+﻿ namespace MagiCloud.KGUI
+{
+    /// <summary>
+    /// 记录者接口
+    /// </summary>
+    public interface IRecord
+    {
+        bool CanReocrd { get; }
+        string[] GetData();
+    }
+}
---
+++ Assets/MagiCloud/KGUI/Scripts/Table/RecordHelper/RecordBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace MagiCloud.KGUI
+{
+    /// <summary>
+    /// 记录者基类
+    /// </summary>
+    public abstract class RecordBase :MonoBehaviour, IRecord
+    {
+
+        public virtual bool CanReocrd => (true);
+
+        public abstract string[] GetData();
+
+    }
+}
