f39c4341da502f6422e89d94bc560c6df9c6dd1f
华清 <uhuaqing@gmail.com>
2018-12-12 18:06:03 +0800

    提交CapOperateType类型脚本

---
+++ Assets/Chemistry/Scripts/Equipments/Other/Caps/CapOperateType.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// 盖子状态类型
+/// </summary>
+public enum CapOperateType
+{
+    /// <summary>
+    /// 瓶子的瓶盖
+    /// </summary>
+    唯一型,
+    /// <summary>
+    /// 支持交互类型的瓶盖
+    /// </summary>
+    交互型,
+    /// <summary>
+    /// 支持连接类型的瓶盖
+    /// </summary>
+    连接型
+}
