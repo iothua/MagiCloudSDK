807d98018592145bc680c221b87dc2b34640b636
华清 <uhuaqing@gmail.com>
2018-12-13 17:25:26 +0800

    提交盖子脚本

---
+++ Assets/Chemistry/Scripts/Equipments/Other/Caps/EO_Cover.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using MagiCloud.Equipments;
+
+namespace Chemistry.Equipments
+{
+    /// <summary>
+    /// 封盖，用于封住仪器
+    /// </summary>
+    public class EO_Cover : EquipmentBase
+    {
+        public CapOperateType capOperate = CapOperateType.唯一型;
+
+        private bool isCover;
+
+        public bool IsCover
+        {
+            get {
+                return isCover;
+            }
+            set
+            {
+
+                if (isCover == value) return;
+                isCover = value;
+
+                if (isCover)
+                {
+                    OpenCover();
+                }
+                else
+                {
+                    CloseCover();
+                }
+            }
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            OnInitializeEquipment();
+        }
+
+        public virtual void OpenCover()
+        {
+
+        }
+
+        /// <summary>
+        /// 关闭盖子（第一次进入）
+        /// </summary>
+        public virtual void CloseCover()
+        {
+
+        }
+    }
+}
+
---
+++ Assets/Chemistry/Scripts/Equipments/Other/Caps/EO_GlassPiece.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Chemistry.Equipments
+{
+    /// <summary>
+    /// 玻璃片
+    /// 1、支持滑动移出
+    /// 2、支持滑动移入
+    /// 3、靠近时，进行交互
+    /// </summary>
+    public class EO_GlassPiece : EO_Cover
+    {
+
+    }
+}
+
