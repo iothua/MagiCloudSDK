3d03f88657a8f5aebb00c4da2ed9528b8f53b1f4
LiiiiiWr <331718478@qq.com>
2019-03-08 16:39:22 +0800

    修改客户端路径指向

--- MCServerProtobuf/Protobuf/myProtoc.bat
+++ MCServerProtobuf/Protobuf/myProtoc.bat
@@ -1,6 +1,6 @@
 @echo off
 cd Proto
-set client_dest_path="..\..\UnityProject\Assets\Network\Protobuf"
+set client_dest_path="..\..UnityClient\Assets\Network\Protobuf"
 set server_dest_path="..\..\MCServer\MCServer\Packer"
 set controller_dest_path="..\..\UnityController\Assets\Network\Protobuf"
 for %%i in (*.*) do protoc --csharp_out=%client_dest_path% %%i
