f9ce16b63f401a473933ee93a1850d826df5b273
iothua <1158078383@qq.com>
2019-03-15 16:41:32 +0800

    添加数据库相关脚本

---
+++ Assets/MagiCloudPlatform/Scripts/Data/Products.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace MagiCloudPlatform.Data
+{
+    /// <summary>
+    /// 产品表
+    /// </summary>
+    public class Products
+    {
+        private int _id;
+
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID {
+            get {
+                return _id;
+            }
+            set {
+                _id = value;
+            }
+        }
+
+        private string _name;
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name {
+            get {
+                return _name;
+            }
+            set {
+                _name = value;
+            }
+        }
+
+        private string _img;
+        /// <summary>
+        /// 图片地址
+        /// </summary>
+        public string Img {
+            get {
+                return _img;
+            }
+            set {
+                _img = value;
+            }
+        }
+
+        private string _summarize;
+        /// <summary>
+        /// 产品概述
+        /// </summary>
+        public string Summarize {
+            get {
+                return _summarize;
+            }
+            set {
+                _summarize = value;
+            }
+        }
+
+        private string _productPath;
+        /// <summary>
+        /// 产品资源路径
+        /// </summary>
+        public string ProductPath {
+            get {
+                return _productPath;
+            }
+            set {
+                _productPath = value;
+            }
+        }
+
+        private string _version;
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        public string Version {
+            get {
+                return _version;
+            }
+            set {
+                _version = value;
+            }
+        }
+
+        private string _experimentPath;
+
+        /// <summary>
+        /// 实验资源路径
+        /// </summary>
+        public string ExperimentPath {
+            get {
+                return _experimentPath;
+            }
+            set {
+                _experimentPath = value;
+            }
+        }
+    }
+}
---
+++ Assets/MagiCloudPlatform/Scripts/Data/UserID.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MagiCloudPlatform.Data
+{
+    public class UserID
+    {
+        private int _userID;
+        public int userID {
+            get { return _userID; }
+            set {
+                _userID = value;
+            }
+        }
+
+        private string _name;
+        public string Name {
+            get {
+                return _name;
+            }
+            set {
+                _name = value;
+            }
+        }
+
+        private string _organization;
+        /// <summary>
+        /// 用户组织
+        /// </summary>
+        public string Organization {
+            get {
+                return _organization;
+            }
+            set {
+                _organization = value;
+            }
+        }
+
+        private string _career;
+        /// <summary>
+        /// 从事职业
+        /// </summary>
+        public string Career {
+            get {
+                return _career;
+            }
+            set {
+                _career = value;
+            }
+        }
+
+        private string _telephone;
+        /// <summary>
+        /// 电话号码
+        /// </summary>
+        public string Telephone {
+            get {
+                return _telephone;
+            }
+            set {
+                _telephone = value;
+            }
+        }
+
+        private string _email;
+        /// <summary>
+        /// 邮件
+        /// </summary>
+        public string Email {
+            get {
+                return _email;
+            }
+            set {
+                _email = value;
+            }
+        }
+
+        private string _permission;
+        public string Permission {
+            get {
+                return _permission;
+            }
+            set {
+                _permission = value;
+            }
+        }
+
+        private string _password;
+        public string Password {
+            get {
+                return _password;
+            }
+            set {
+                _password = value;
+            }
+        }
+    }
+}
+
