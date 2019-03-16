90581159b8fea79486211c8f03fd99768d776ac2
成华清 <uhuaqing@gmail.com>
2019-03-16 11:19:53 +0800

    准备添加下载功能

---
+++ Assets/MagiCloud/Scripts/Downloads/MDownloader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MagiCloud.Downloads
+{
+    /// <summary>
+    /// 下载功能
+    /// </summary>
+    public class MDownloader : MonoBehaviour
+    {
+       
+    }
+}
+
