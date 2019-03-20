f56d715bd44a121f6e71070698e4387442318700
LiiiiiWr <331718478@qq.com>
2019-03-20 12:00:47 +0800

    加入NetEventPool

---
+++ Assets/MagiCloud/NetWorks/Scripts/Events/EventPool.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MagiCloud.NetWorks
+{
+    /// <summary>
+    /// 事件池，获取或实例化事件
+    /// </summary>
+    public class EventPool
+    {
+        ServerConnection onnection;
+        private List<EventBase> events;
+
+        public EventPool(ServerConnection onnection)
+        {
+            this.onnection=onnection;
+            events=new List<EventBase>();
+        }
+
+        /// <summary>
+        /// 获取事件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T GetEvent<T>() where T : EventBase
+        {
+            Type type = typeof(T);
+            foreach (var item in events)
+            {
+                if (item.GetType()==type)
+                    return item as T;
+            }
+            return CreateEvent(type)as T;
+        }
+
+        /// <summary>
+        /// 移除事件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public bool RemoveEvent<T>() where T : EventBase
+        {
+            Type type = typeof(T);
+            foreach (var item in events)
+            {
+                if (item.GetType()==type)
+                {
+                    item.Remove();
+                    events.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 实例化事件
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private EventBase CreateEvent(Type type)
+        {
+            EventBase eventBase = (EventBase)Activator.CreateInstance(type);
+            if (eventBase==null)
+                throw new Exception("实例化类型失败:"+type.FullName);
+            events.Add(eventBase);
+            eventBase.Init(onnection);
+            return eventBase;
+        }
+    }
+
+
+}
